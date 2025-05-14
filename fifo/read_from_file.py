f7c130c1dfc687e13000f131dde7722ef2c28b36
onionmccabbage <onionmccabbage@live.com>
2025-05-14 16:00:03 +0100

    using file access to read and readlines

---
+++ fifo/read_from_file.py
@@ -0,0 +1,23 @@
+# We may read back from a file
+def readfile():
+    try:
+        with open('my_file.txt', 'rt') as fin: # 'r' will read ('t' for text)
+            retrieved = fin.read() # retrieve the entire contents
+            return retrieved
+    except Exception as err:
+        print(err)
+
+def readFileLines():
+    '''retrieve the contents into a list of lines'''
+    try:
+        with open('my_file.txt', 'rt') as fin: # 'r' will read ('t' for text)
+            retrieved = fin.readlines() # retrieve the entire contents as a list of lines
+            return retrieved
+    except Exception as err:
+        print(err)
+
+if __name__ == '__main__':
+    r = readfile()
+    print(r)
+    l = readFileLines()
+    print(l) # we have a list
