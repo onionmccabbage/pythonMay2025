7b4c3e25933d9a628425f601ea642a23f84a4b0d
onionmccabbage <onionmccabbage@live.com>
2025-05-14 16:26:16 +0100

    using sys argv to configure code

--- fifo/write_to_file.py
+++ fifo/write_to_file.py
@@ -1,10 +1,16 @@
+import sys
 # sometimes we need to write rather than print
 # Whenever we deal with I/O bound events it is a good idea to use try/except
 def commitTofile(txt): # we may choose to receive zero or more arguments
     '''write data to a text file'''
     try:
+        # if there is a file name is sys.argv the use it
+        if len(sys.argv) > 1:
+            file_to_write = sys.argv[1]
+        else:
+            file_to_write = 'my_file.txt'
         # 'with' is a very tidy way to work with objects
-        with open('my_file.txt', 'at') as fout:
+        with open(file_to_write, 'at') as fout:
             fout.write(txt) # commit the incoming text to this file access object
             # we may choose ot add a new line character
             fout.write('\n') # \n is encoding for a new line. \t is tab
