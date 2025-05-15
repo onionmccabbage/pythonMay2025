8047e8ab4ec8d0687dc7f3f2a2b569a3f0e3d3be
onionmccabbage <onionmccabbage@live.com>
2025-05-15 12:27:05 +0100

    importing our own modules

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # this will be our main module
+from ts import getDate
+from ts import getTimeStamp
+# from ts import getDate, getTimeStamp
+from util import makeInt
+
+def main():
+    ''' we often write a main module'''
+    d = getDate()
+    t = getTimeStamp()
+    i = makeInt(42.24)
+    print(d, t, i)
+
+if __name__ == '__main__':
+    main()
