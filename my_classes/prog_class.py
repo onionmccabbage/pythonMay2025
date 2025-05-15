85b8e107122df02282e11d5a2e0add8c271c3406
onionmccabbage <onionmccabbage@live.com>
2025-05-15 11:58:55 +0100

    testing the validation is orperating ok

--- my_classes/prog_class.py
+++ my_classes/prog_class.py
@@ -32,6 +32,11 @@ class Simple(): # the brackets are optional
         else:
             raise TypeError('episode must be a positive integer')
 
+# we can create our own exception classes
+class NotPosInt(TypeError):
+    pass
+
+
 class Digital(Simple): # this new clas inherits all the capabilities of the parent class
     '''we extend the simple class to include attributes related to digital'''
     def __init__(self, name, episode, filesize):
@@ -43,6 +48,15 @@ s1 = Simple('Play School', 32)
 s2 = Simple('News', 5667)
 # NB s.name will call the getter function for 'name'
 print(s1, type(s1), s1.name, s1.episode)
+
+# we can check if our validation is operating correctly
+# an incorrect name
+try:
+    # s3 = Simple(True, 55) # True should not be permitted
+    # s4 = Simple('totp', None) # none shoyuld not be permitted
+    s5 = Simple('totp', -99) # -ve not be permitted
+except TypeError as te:
+    print(te)
 
 d1 = Digital('Prog', 34, 564745664)
 
