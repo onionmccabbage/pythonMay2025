e6b90c3845e8b4eea72be54c8f41169bbfe9d89e
onionmccabbage <onionmccabbage@live.com>
2025-05-15 16:08:11 +0100

    using generator

--- using_comp.py
+++ using_comp.py
@@ -22,7 +22,21 @@ dt = datetime.time(23)
 print(dt)
 dates_l = [datetime.time(i) for i in raw_data]
 print(dates_l) # we have a nice list of actual time stamps
+for dt in dates_l:
+    print(dt)
 
 # generator syntax
+p = (i**2 for i in range(0,101)) # this is not a list, it is a generator
+print(type(p))
+# a generator lets us iterate over the generated members 
+# but the members do not persist in memory
+for i in p:
+    print(i)
 
+# use-cases
+# generate the next id for a file/db/API etc
+# automatically generate the next file name for storage/retrieval
+names_g = (f'log_{i}.json' for i in range(0,11))
+for i in names_g:
+    print(i)
 
