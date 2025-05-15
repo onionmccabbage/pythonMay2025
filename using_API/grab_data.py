a85e62439ff4033aa22f6b6293a2b7f0822ebddb
onionmccabbage <onionmccabbage@live.com>
2025-05-15 10:16:42 +0100

    getting API data and using sys.argv

--- using_API/grab_data.py
+++ using_API/grab_data.py
@@ -3,17 +3,27 @@
 # https://jsonplaceholder.typicode.com/users/1
 
 import requests # we may need to pip install requests
+import sys
 
-
-def getData():
+def getData(id=1):
     '''fetch some JSON from an API'''
-    apiURL = 'https://jsonplaceholder.typicode.com/users/1'
+    apiURL = f'https://jsonplaceholder.typicode.com/users/{id}'
     response = requests.get(apiURL) # we now have a response object
     # the response object will contain a status code, headers, the URL and our data
     # data may be JSON, HTML, Text, XML ....
     data = response.json() # since our data is JSON this will give us what we need
-    return data
+    return data # we have a dictionary containing the data
+# () usually means a tuple
+# [] usually means a list
+# {} usually means a dict or a set
 
 if __name__ == '__main__':
-    user = getData()
+    # where to get arguments...
+    # user input, read from file, get a sys.argv, grab from database....
+    if len(sys.argv)>1:
+        chosenID = sys.argv[1]
+    else:
+        chosenID = 3
+
+    user = getData(chosenID) # we may choose to pass an id argument
     print(user)
