e7c1d3022e5083ca73aaf98e0f7bef45270b9996
onionmccabbage <onionmccabbage@live.com>
2025-05-15 15:54:23 +0100

    using range and list comprehension

--- using_comp.py
+++ using_comp.py
@@ -1,3 +1,5 @@
+import datetime
+
 # the range object is extremely efficient
 r = range(-10, 11, 2) # start, stop-before, step
 for i in r:
@@ -10,8 +12,16 @@ print(s[2:9:2]) # [start:stop-before:step]
 print(t[0:6])
 
 # list comprehension
+square_nums = [i**2 for i in range(0,101)]
+print(square_nums)
 
-
+# comprehension use-case
+raw_data = (7,4,8,3,9,22,5)
+# maybe we need the date representation of these numbers
+dt = datetime.time(23)
+print(dt)
+dates_l = [datetime.time(i) for i in raw_data]
+print(dates_l) # we have a nice list of actual time stamps
 
 # generator syntax
 
