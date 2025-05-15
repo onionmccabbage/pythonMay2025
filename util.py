0775695b6186943c4c3e975ebe6c3a5745d583a1
onionmccabbage <onionmccabbage@live.com>
2025-05-15 12:22:44 +0100

    simple utility function

--- util.py
+++ util.py
@@ -1,1 +1,9 @@
-# here we write some useful utilites
+# here we write a useful utility
+
+def makeInt(n):
+    '''if a valid number is provided, return the integer part
+    for invalid values, return zero'''
+    if type(n) in (int, float):
+        return int(n)
+    else:
+        return 0
